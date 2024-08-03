cf309be7dde96cada16571ed9f74abdcdfc255e0
magame2310 <161406463+magame2310@users.noreply.github.com>
2024-08-03 04:50:10 -0500

    Add files via upload

---
+++ Ejercicio propuesto 19.py
@@ -0,0 +1,5 @@
+lado_triangulo_equilatero = float(input('Ingrese el valor del lado del triángulo equilátero'))
+
+print('Perímetro del triángulo equilatero: ', 3 * lado_triangulo_equilatero)
+print('Altura del triángulo equilatero: ', lado_triangulo_equilatero * (3 ** (1/2)) / 2)
+print('Área del triángulo equilatero: ', (lado_triangulo_equilatero ** 2) * (3 ** (1/2)) / 4)
---
+++ Ejercicio resuelto 7.py
@@ -0,0 +1,9 @@
+a = float(input('Ingrese el valor de a'))
+b = float(input('Ingrese el valor de b'))
+
+if a > b:
+  print('a es mayor que b')
+elif a < b:
+  print('a es menor que b')
+else:
+  print('a es igual a b')
